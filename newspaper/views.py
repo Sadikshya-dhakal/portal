37405e5ecead5acd13f0f6488b59881553674972
Sadikshya Dhakal <dhakalsadikshya52@gmail.com>
2025-11-24 19:01:08 +0545

    url

--- newspaper/views.py
+++ newspaper/views.py
@@ -77,7 +77,4 @@ class PostDetailView(DetailView):
            published_at__isnull=False, status="active"
         ).order_by("-published_at")[:5]
         return context
-        
-        
-
-        
+    
